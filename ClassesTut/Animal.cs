d6aa2b8a99ca25f9ec25f6379064ee5634c7514b
Huego <hugozaanen@hotmail.com>
2019-03-03 21:53:25 +0100

    Animal class streamlined, and some exercies

--- ClassesTut/Animal.cs
+++ ClassesTut/Animal.cs
@@ -1,46 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ClassesTut
 {
     class Animal
     {
-        public string name;
-        public string sound;
+        private string name;
+        private string sound;
 
-        public Animal()
-        {
-            name = "No Name";
-            sound = "No Sound";
-            numOfAnimals++;
-        }
+        public const string SHELTER = "Derek's home for Animals";
 
-        public Animal(string name = "No name")
-        {
-            this.name = name;
-            this.sound = "no Sound";
-            numOfAnimals++;
-        }
-
-        public Animal(string name = "No name",
-            string sound = "No sound")
-        {
-            this.name = name;
-            this.sound = sound;
-        }
+        public readonly int idNum;
 
         public void MakeSound()
         {
-            Console.WriteLine("{0} says {1}",
-                name,sound);
+            Console.WriteLine("{0} says {1}",name,sound);
         }
 
-        static int numOfAnimals = 0;
+        public Animal()
+            : this("No Name", "No Sound") { }
 
-        public static int GetNumAnimals()
+        public Animal(string name)
+            : this(name, "No sound") { }
+
+        public Animal(string name, string sound)
         {
-            return numOfAnimals;
+            SetName(name);
+            Sound = sound;
+
+            NumOfAnimals = 1;
+
+            Random rnd = new Random();
+            idNum = rnd.Next(1,2147483640);
+
+        }
+
+        public void SetName(string name)
+        {
+            if(!name.Any(char.IsDigit))
+            {
+                this.name = name;
+            }else
+            {
+                this.name = "No Name";
+                Console.WriteLine("Name can't contain numbers");
+            }
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string Sound
+        {
+            get { return sound; }
+            set
+            {
+                if(value.Length > 10)
+                {
+                    sound = "No Sound";
+                    Console.WriteLine("Sound is too long");                    
+                }
+                else
+                {
+                    sound = value;
+                }
+            }
+        }
+
+        public string Owner { get; set; } = "No Owner";
+
+        public static int numOfAnimals = 0;
+
+        public static int NumOfAnimals
+        {
+            get { return numOfAnimals; }
+            set { numOfAnimals += value; }
         }
     }
 }
--- ClassesTut/Program.cs
+++ ClassesTut/Program.cs
@@ -19,20 +19,43 @@ namespace ClassesTut
             //    Animal.GetNumAnimals());
             #endregion
 
+            #region
             //Console.WriteLine("Area of Rectangle : {0}",
             //    ShapeMath.GetArea("rectangle",5,6));
 
-            int? randNum = null;
+            //int? randNum = null;
 
-            if(randNum == null)
-            {
-                Console.WriteLine("randNum is null");
-            }
+            //if(randNum == null)
+            //{
+            //    Console.WriteLine("randNum is null");
+            //}
 
-            if (!randNum.HasValue)
-            {
-                Console.WriteLine("randNum is null");
-            }
+            //if (!randNum.HasValue)
+            //{
+            //    Console.WriteLine("randNum is null");
+            //}
+            #endregion
+
+            Animal cat = new Animal();
+
+            cat.SetName("Whiskers");
+            cat.Sound = "Meow";
+
+            Console.WriteLine("The cat is named {0} and says {1}",
+                cat.GetName(),cat.Sound);
+
+            cat.Owner = "Derek";
+
+            Console.WriteLine("{0} owner is {1}",
+                cat.GetName(), cat.Owner);
+
+            Console.WriteLine("{0} shelter id is {1}",
+                cat.GetName(),cat.idNum);
+
+            Animal fox = new Animal("Oscar","Rawrrr");
+
+            Console.WriteLine("# of Animals : {0}",
+                Animal.NumOfAnimals);
 
             Console.ReadLine();
         }
