3407db369644033e7bb71c7dbb691e0071854f84
Huego <hugozaanen@hotmail.com>
2019-03-03 20:07:02 +0100

    structs exercise

--- ClassesTut/Program.cs
+++ ClassesTut/Program.cs
@@ -6,7 +6,40 @@ namespace ClassesTut
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Rectangle rect1;
+            rect1.length = 200;
+            rect1.width = 50;
+            Console.WriteLine("Area of rect1 : {0}",
+                rect1.Area());
+
+            Rectangle rect2 = new Rectangle(100,40);
+
+            rect2 = rect1;
+            rect1.length = 33;
+
+            Console.WriteLine("rect2.length : {0}",
+                rect2.length);
+
+
+            Console.ReadLine();
+        }
+
+        struct Rectangle
+        {
+            public double length;
+            public double width;
+
+            public Rectangle(double l = 1,
+                double w = 1)
+            {
+                length = l;
+                width = w;
+            }
+
+            public double Area()
+            {
+                return length * width;
+            }
         }
     }
 }
